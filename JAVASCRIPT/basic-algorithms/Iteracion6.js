5a0b847e0d74d51d6cb955a14cce874e351130d4
Javi <javi_mm89@live.com>
2022-10-01 22:47:00 +0200

    Iteracion6

--- JAVASCRIPT/basic-algorithms/Iteracion6.js
+++ JAVASCRIPT/basic-algorithms/Iteracion6.js
@@ -1,28 +1,31 @@
-//Iteración 6
+//Iteración 6: Bucles
 //1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
- for (i = 0; i <= 9; i++) {
-     console.log(i);
- }
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-
-//1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
-//cuando el resto del numero dividido entre 2 sea 0.
-for (i = 0; i <= 9; i++){
-  if(i % 2 == 0) {
-    console.log(i);
-  }
+for (let index = 0; index < numbers.length; index++) {
+  console.log(index);
 }
 
+//1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo
+//cuando el resto del numero dividido entre 2 sea 0.
+const numbers1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (i = 0; i <numbers1.length; i++){
+    if(i % 2 == 0) {
+      console.log(i);
+    }
+  }
 
-//1.3 Crea un bucle para conseguir dormir contando ovejas. 
+
+//1.3 Crea un bucle para conseguir dormir contando ovejas.
 //Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
-//Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
+//Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle
 //y cambia el mensaje en la décima vuelta a 'Dormido!'.
-for (i = 0; i <= 10; i++){
-  if (i == 10){
-    console.log("Dormido!")
-    
-  } else if (i == 0,9) {
+const sheeps = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (i = 0; i < sheeps.length; i++) {
+  if (i == 10) {
+    console.log("Dormido!");
+  } else if ((i == 0, 9)) {
     console.log("Intentando dormir 🐑");
   }
 }
