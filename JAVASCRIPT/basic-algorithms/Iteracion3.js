25e43dab766b589c27b95440ac13eb499fcd734c
Javi <javi_mm89@live.com>
2022-10-01 22:31:21 +0200

    Iteracion3

--- JAVASCRIPT/basic-algorithms/Iteracion3.js
+++ JAVASCRIPT/basic-algorithms/Iteracion3.js
@@ -1,30 +1,28 @@
-//Iteración 3
+//Iteración 3: Operadores
 
 //1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
-let multiplicar = 10 * 5;
-console.log(multiplicar);
+const multiplication = 10 * 5;
+console.log(multiplication)
 
 //1.2 Divide 10 por 2 y muestra el resultado en un console.
-let dividir = 10 / 2;
-console.log(dividir);
+ const divide = 10/2;
+ console.log(divide)
 
 //1.3 Muestra mediante un console el resto de dividir 15 por 9.
-let a = 15;
-let b = 9;
-let resultado = a/b;
-let resto = a%b
-console.log(resto);
+ const divided = 15/9;
+ const resto = 15%9;
+ console.log('División', divided , ' Resto' , resto)
 
 //1.4 Usa el correcto operador de asignación que resultará en o = 15, 
 //teniendo dos variables p = 10 y j = 5.
-const p = 10;
-const j = 5;
-const total = (p + j);
-console.log(total);
+ const p = 10;
+ const j = 5;
+ const o = (p + j); 
+ console.log(o)
 
 //1.5 Usa el correcto operador de asignación que resultará en i = 50,
 //teniendo dos variables c = 10 y m = 5.
-const c = 10;
-const m = 5;
-const result = (c * m);
-console.log(result);
+ const c = 10;
+ const m = 5;
+ const i = (c * m);
+ console.log(i)
